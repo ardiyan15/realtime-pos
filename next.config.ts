5d089bdd823bf36de9aad85f5b1a123c3039db3c
ardiyan15 <ardhiyan15@gmail.com>
2025-08-07 15:55:08 +0700

    create upload image feature

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb'
+    }
+  },
+
+  devIndicators: false,
+  images: {
+    domains: ["https://hlnyevvoylahyatjsnbc.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hlnyevvoylahyatjsnbc.supabase.co',
+        port: '',
+        pathname: '/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
