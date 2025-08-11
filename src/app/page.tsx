8d3ecebc26a7eb4f27ec79c00ccb86a546b94762
ardiyan15 <ardhiyan15@gmail.com>
2025-08-11 15:13:58 +0700

    create update user feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
-      <h1 className="text-4xl font-semibold">Welcom Ardiyan Agus Prayoga</h1>
+      <h1 className="text-4xl font-semibold">Welcome Ardiyan Agus Prayoga</h1>
       <Link href="/admin">
         <Button className="bg-teal-500 text-white">Access Dashboard</Button>
       </Link>
