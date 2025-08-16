bb9899781bf1a61104bcd1125b57edf511fb4edd
ardiyan15 <ardhiyan15@gmail.com>
2025-08-16 14:02:49 +0700

    add table management list

---
+++ src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,9 @@
+import TableManagemenet from "./_components/table";
+
+export const metadata = {
+  title: "My Cafe | Table Management",
+};
+
+export default function MenuManagementPage() {
+  return <TableManagemenet />;
+}
