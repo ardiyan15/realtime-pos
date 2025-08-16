6b2f15c01407f4d4af6e8e367128cf7eee409b70
ardiyan15 <ardhiyan15@gmail.com>
2025-08-16 14:37:23 +0700

    add create table management

---
+++ src/types/table.d.ts
@@ -0,0 +1,11 @@
+export type TableFormState = {
+    status?: string;
+    errors?: {
+        id?: string[];
+        name?: string[];
+        description?: string[];
+        status?: string[];
+        capacity?: string[];
+        _form?: string[];
+    };
+};
