ae2e5a449fb5d8fb6ca3a9fa9312a86686beb4d3
ardiyan15 <ardhiyan15@gmail.com>
2025-10-02 13:53:36 +0700

    create chart in dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,9 @@
-export default function AdminDashboardPage() {
-    return (
-        <div>
-            <button>Dashboard</button>
-        </div>
-    )
+import Dashboard from "./_components/dashboard";
+
+export const metadata = {
+  title: "My Cafe | Table Management",
+};
+
+export default function DashboardPage() {
+  return <Dashboard />;
 }
